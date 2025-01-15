14e691b2323f03749b4db4b69d2bedbbf9ff1790
svenku <sven.kunsing@gmail.com>
2025-01-16 00:42:20 +0200

    get pygame screen and base game loop working

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
-import constants
+from constants import *
 
 def main():
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   print('Starting asteroids!')
-  print(f'Screen width: {constants.SCREEN_WIDTH}')
-  print(f'Screen height: {constants.SCREEN_HEIGHT}')
+  print(f'Screen width: {SCREEN_WIDTH}')
+  print(f'Screen height: {SCREEN_HEIGHT}')
+  while True:
+    screen.fill(BLACK)
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        return
+
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
