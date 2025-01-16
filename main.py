d899ce60f69704acc64166d12fcf6ef776c6e431
svenku <sven.kunsing@gmail.com>
2025-01-16 02:17:54 +0200

    add player rotation

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
   dt = 0
   
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  player = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
   
   print('Starting asteroids!')
   print(f'Screen width: {SCREEN_WIDTH}')
@@ -16,18 +17,19 @@ def main():
 
   while True:
     
-    screen.fill(BLACK)
-    player = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
-    player.draw(screen)
-    
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
         return
     
-    dt = clock.tick(60)/1000
-    
-    pygame.display.flip()
+    screen.fill(BLACK)
 
+    player.update(dt)    
+    player.draw(screen)    
+
+    pygame.display.flip()
+    dt = clock.tick(60)/1000
+
+    
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, WHITE, PLAYER_LINE_WIDTH
+from constants import PLAYER_RADIUS, WHITE, PLAYER_LINE_WIDTH, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -18,7 +18,15 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, WHITE, self.triangle(), PLAYER_LINE_WIDTH)
 
-    # def update(self, dt):
-    #     self.position += self.velocity * dt
-    #     self.rect.center = self
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        self.rotation %= 360
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
