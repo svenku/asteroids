901b30993df3196e512e835a85223c3bb9873b23
svenku <sven.kunsing@gmail.com>
2025-01-16 02:46:56 +0200

    bring in updatable, drawable sprite groups, add player to those

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from player import Player
 
 def main():
   pygame.init()
-  
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
   dt = 0
-  
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+
+  Player.containers = (updatable, drawable)
+
   player = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
   
   print('Starting asteroids!')
@@ -21,15 +25,18 @@ def main():
       if event.type == pygame.QUIT:
         pygame.quit()
         return
+
+    for obj in updatable:
+      obj.update(dt)
     
     screen.fill(BLACK)
-
-    player.update(dt)    
-    player.draw(screen)    
+    
+    for obj in drawable:
+      obj.draw(screen)  
 
     pygame.display.flip()
     dt = clock.tick(60)/1000
 
-    
+
 if __name__ == "__main__":
     main()
